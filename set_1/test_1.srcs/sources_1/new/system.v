ebc58f19f7d54e988854c9b9b7e6cb8566933825
tan14007 <panukorn1400@naver.com>
2020-03-11 11:49:45 +0700

    Refactor Set 1 Code

--- set_1/test_1.srcs/sources_1/new/system.v
+++ set_1/test_1.srcs/sources_1/new/system.v
@@ -6,35 +6,36 @@ module system(
     output [6:0]seg
 );
 
-wire [30:0]clkDiv;
-assign clkDiv[0] = clk;
+wire [30:0] clkDiv;
+reg  [1:0]  ledState    = 3;    // Starting 7-segment enable state with leftmost display 
+reg  [3:0]  num         = 10;   // 4-bits register, starting state with A
+assign      clkDiv[0]   = clk;  // Assign base clock
+assign      dp          = 1;    // Making dot disappear
 
 genvar i;
 for(i=1;i<=30;i=i+1)
     clockDiv(clkDiv[i-1], clkDiv[i]);
 
-reg [6:0]clkSelect = 24;
-assign targetClk = clkDiv[clkSelect];
 
-reg [1:0]ledState = 3;
-reg [3:0]num = 10;
-reg [5:0]ct=4;
-reg [3:0]num0,num1,num2,num3;
 sevenSeg(num, seg);
-assign dp = 1;
-always @(posedge clkDiv[18])
-begin
-if(sw[0]) begin an = 4'b1111; an[ledState] = 0; end
-else an = 4'b0000;
-end
-always @(posedge targetClk)  begin
-case (num)
-10: num = 12;
-12: num = 14;
-14: num = 10;
-endcase
-ledState = (ledState-1);
-end
 
+always @(posedge clkDiv[18])
+begin        
+    if(sw[0]) // If turn sw[0] on, display only one LED
+    begin 
+        an = 4'b1111; 
+        an[ledState] = 0; 
+    end
+    else 
+        an = 4'b0000; // Else, display all
+    
+    case (num)  // For simplicity in code, we will use case for state changing
+        10: num = 12;
+        12: num = 14;
+        14: num = 10;
+    endcase
+    
+    ledState = (ledState-1);
+end
 endmodule
     
