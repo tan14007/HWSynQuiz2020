84660dbdf3d6dcc9eb3803f897449e21c1fa587b
tan14007 <panukorn1400@naver.com>
2020-03-11 13:50:23 +0700

    Add Set 4 solutiotion

---
+++ set_4/set_4_copy.srcs/sources_1/new/sevenSeg.v
@@ -0,0 +1,34 @@
+// 7-segment encoding
+//      0
+//     ---
+//  5 |   | 1
+//     --- <--6
+//  4 |   | 2
+//     ---
+//      3
+module sevenSeg(
+    input [3:0]num,
+    output reg [6:0]seg
+    );
+always @(num)
+begin
+      case (num)
+          4'b0001 : seg = 7'b1001111;   // 1, editted to enable 4th and 5th segment instead of 1st and 2nd
+          4'b0010 : seg = 7'b1111011;   // 2
+          4'b0011 : seg = 7'b1110111;   // 3
+          4'b0100 : seg = 7'b1101111;   // 4
+          4'b0101 : seg = 7'b1011111;   // 5
+          4'b0110 : seg = 7'b0111111;   // 6
+          4'b0111 : seg = 7'b1111000;   // 7
+          4'b1000 : seg = 7'b0000000;   // 8
+          4'b1001 : seg = 7'b0010000;   // 9
+          4'b1010 : seg = 7'b0001000;   // A
+          4'b1011 : seg = 7'b0000011;   // b
+          4'b1100 : seg = 7'b1000110;   // C
+          4'b1101 : seg = 7'b0100001;   // d
+          4'b1110 : seg = 7'b0000110;   // E
+          4'b1111 : seg = 7'b0001110;   // F
+          default : seg = 7'b1111110;   // 0
+      endcase
+      end
+     endmodule
---
+++ set_4/set_4_copy.srcs/sources_1/new/system.v
@@ -0,0 +1,54 @@
+module system(
+    input clk,
+    input btnD,
+    output dp,
+    output reg [3:0]an,
+    output [6:0]seg
+);
+
+wire [30:0]clkDiv;
+assign clkDiv[0] = clk;
+
+reg [31:0]curNum = 123;
+reg [4:0]curAddNum = 4;
+reg [3:0]num = 0;
+reg [1:0]ledState = 0;
+reg [2:0]nextLedState = 0;
+reg [5:0]clkSelect = 25;
+assign dp = 1;
+
+genvar i;
+for(i=1;i<=30;i=i+1)
+    clockDiv(clkDiv[i-1], clkDiv[i]);
+
+
+sevenSeg(num, seg);
+assign dp = 1;
+
+always @(posedge clkDiv[18])
+begin
+    an = 4'b1111;
+    an[ledState] = 0;
+    ledState = ledState+1;
+    
+    case(ledState)
+        0: num = curNum%10;
+        1: num = (curNum/10)%10;
+        2: num = (curNum/100)%10;
+        3: num = curNum/1000;
+    endcase
+    
+    if(btnD) clkSelect = 27;
+    else clkSelect = 25;
+ 
+end
+
+always @(posedge clkDiv[clkSelect])
+begin
+    if(curNum <= 9) curNum = ((curNum*10)+curAddNum)%1000;
+    else if(curNum != 1920) curNum = ((curNum*100)+curAddNum)%1000;
+    if(curAddNum <= 19) curAddNum = curAddNum + 1;
+end
+
+endmodule
+    
